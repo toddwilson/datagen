b6e6e4c2a2cfb49580a9d6e6c7225040f42088c2
Todd Wilson <todd@toddwilson.net>
2014-07-12 20:53:02 -0400

    setup.py additions

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 from setuptools import setup, find_packages
 
+VERSION = '1.0.0'
+
 with open('README.rst') as f:
-    readme = f.read()
+    README = f.read()
 
 entrypoints = {
     'console_scripts': 'datagen = datagen:main'
 }
 
 setup(
-      name='datagen',
-      version='1.0.0',
-      description='Generate delimited sample data with a simple schema.',
-      long_description=readme,
-      author='Todd Wilson',
-      author_email='todd@toddwilson.net',
-      license='Apache License 2.0',
-      url='https://github.com/toddwilson/datagen',
-      entry_points=entrypoints,
-      include_package_data = True,
-      packages=find_packages('datagen'),
-      classifiers=(
+    name='datagen',
+    version=VERSION,
+    description='Generate delimited sample data with a simple schema.',
+    long_description=README,
+    author='Todd Wilson',
+    author_email='todd@toddwilson.net',
+    license='Apache License 2.0',
+    url='https://github.com/toddwilson/datagen',
+    download_url='https://github.com/toddwilson/datagen/tarball/%s' % VERSION,
+    keywords=['data generation', 'sample data', 'hadoop'],
+    entry_points=entrypoints,
+    include_package_data=True,
+    packages=find_packages('datagen'),
+    classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
